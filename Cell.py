8707bf6b217cb9aae9f37a7dd40c9d90e80ea847
ratinmyravioli <erikapoirier@ufl.edu>
2025-04-26 18:58:34 -0400

    Put back font color to black

--- Cell.py
+++ Cell.py
@@ -19,7 +19,7 @@ class Cell:
     def draw(self):
         if self.value != 0:
             number_font = pygame.font.SysFont("Arial", 10)
-            number_text = number_font.render(f"{self.value}", True, grey)
+            number_text = number_font.render(f"{self.value}", True, "black")
             self.screen.blit(number_text, (self.row,self.col))
 
 
