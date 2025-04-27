a3854165dcc56e9ee55c04e46c76e8fb439a0c6c
maxin <mountainfalcon15@gmail.com>
2025-04-26 20:05:30 -0400

    FIGURED IT OUT because im the GOAT

--- Cell.py
+++ Cell.py
@@ -23,8 +23,8 @@ class Cell:
             number_text = number_font.render(f"{self.value}", True, black)
             self.screen.blit(number_text, ((self.col) * 78 +30,(self.row) * 78 + 20))
         elif self.sketched_value != 0:
-            number_font = pygame.font.SysFont("Arial", 20)
-            number_text = number_font.render(f"{self.value}", True, gray)
+            number_font = pygame.font.SysFont("Arial", 30)
+            number_text = number_font.render(f"{self.sketched_value}", True, gray)
             self.screen.blit(number_text, ((self.col) * 78 + 5, (self.row) * 78 + 3))
 
 
